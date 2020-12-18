1212b05d3354171ac003e0525e1a71fc90b038eb
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-12-18 13:55:12 +0100

    animate

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,24 @@
+const scrolledNav = () => {
+  const intervention = document.querySelector('#js-animation');
+  const element = document.querySelector('#js-animation2');
+  if (intervention) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 250) {
+        intervention.classList.remove('d-none');
+        intervention.classList.add('animate__animated', 'animate__backInLeft');
+      }
+    });
+  }
+  if (element) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 800) {
+        element.classList.remove('d-none');
+        element.classList.add('animate__animated', 'animate__backInLeft');
+      }
+    });
+  }
+}
+
+
+
+export { scrolledNav }
