d4da95bf68ea0594bd697b0f7c73a92a80b8ebd5
Emmanuel ROSE <roseemmanuel@hotmail.com>
2021-02-23 00:27:20 +0100

    sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'poncetsas', to: 'pages#poncetsas'
   get 'aze', to: 'pages#aze'
+  get 'lfelec', to: 'pages#lfelec'
+  get 'mattard', to: 'pages#mattard'
+  get 'entreprisemarie', to: 'pages#entreprisemarie'
+  get 'deniaux_couverture', to: 'pages#deniaux_couverture'
 
   get 'mention', to: 'pages#mention'
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
